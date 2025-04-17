e56fcf1b22267ecbc3fa8120d0bd753c71211159
Nadia-Momo <nadiamahbuba.webdevsoln@gmail.com>
2025-04-18 00:22:14 +0600

    finally banner part done

---
+++ js/script.js
@@ -0,0 +1,15 @@
+var typed =new Typed(".auto-type",{
+
+    strings:["THE FUTURE.","THE SYSTEM.","THE BIKE."],
+    typeSpeed:150,
+    backSpeed:150,
+    loop:true
+});
+$('.multiple-items').slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows:false,
+    autoplay:true,
+    autoplaySpeed:1000
+  });
