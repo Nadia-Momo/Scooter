ff1695a9d50e8335a494d539b1a7722fe89e7ffd
Labib361 <ahsanulmahbub1999@gmail.com>
2025-04-21 14:48:54 +0600

    Finally the scooter project done

--- js/script.js
+++ js/script.js
@@ -6,8 +6,6 @@ var typed =new Typed(".auto-type",{
     loop:true
 });
 
-
-
   $('.multiple-items').slick({
     infinite: true,
     slidesToShow: 1,
@@ -23,5 +21,28 @@ var typed =new Typed(".auto-type",{
     delay: 100,
     time: 1000
 });
-
-  
+// colors slider
+$('.slider-for').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: false,
+  fade: true,
+  asNavFor: '.slider-nav'
+});
+$('.slider-nav').slick({
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  asNavFor: '.slider-for',
+  dots: false,
+  centerMode: true,
+  focusOnSelect: true,
+  arrows:false
+});
+		
+$('.my_slider').slick({
+  infinite: true,
+  slidesToShow: 3,
+  slidesToScroll: 3,
+  prevArrow:'<i class="fa-solid arrows prr fa-arrow-left"></i>',
+    nextArrow:'<i class="fa-solid arrows nrr fa-arrow-right"></i>'
+});
