4a275424ffc1a0867a3eea5265b38d680ce91ba1
Labib361 <ahsanulmahbub1999@gmail.com>
2025-04-27 02:46:06 +0600

    overflow hidden done

--- js/script.js
+++ js/script.js
@@ -11,8 +11,8 @@ var typed =new Typed(".auto-type",{
     slidesToShow: 1,
     slidesToScroll: 1,
     arrows:false,
-    autoplay:true,
-    autoplaySpeed:1000
+    autoplay:false
+    // autoplaySpeed:1000
   });
 
   AOS.init();
